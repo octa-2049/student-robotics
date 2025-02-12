a92c8081408c20e23e3e7a2d5a442087068eae94
ARK1309 <ameyakotian19@gmail.com>
2025-02-12 17:15:54 +0000

    Add files via upload

---
+++ robot.py
@@ -0,0 +1,21 @@
+import math
+
+from myrobot import MyRobot
+
+myRobot = MyRobot()
+
+while True:
+
+    # myRobot.turn(0.2)
+    # myRobot.targetIDs = myRobot.palletIDs
+    # myRobot.look(80)
+    # print(myRobot.markerInfo)
+    #myRobot.goToBoxLong(80)
+    mark = (myRobot.look(myRobot.palletIDs))
+    if mark != None:
+        print(mark.id)
+        roll = mark.orientation.roll
+        roll = myRobot.roundRollDeg(roll)
+        print(mark.orientation.roll)
+        print((mark.orientation.yaw))
+        print((mark.orientation.pitch))
