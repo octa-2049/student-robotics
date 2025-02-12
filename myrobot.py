a177fcc5dfdc4548984f2c6b28329bdfba5c5106
ARK1309 <ameyakotian19@gmail.com>
2025-02-12 17:11:57 +0000

    Create myrobot.py

---
+++ myrobot.py
@@ -0,0 +1,202 @@
+import math
+from sr.robot3 import *
+
+class MyRobot(Robot):
+    def __init__(self):
+        super().__init__()
+
+        self.SPEED = 0.2
+        self.DIAMETER = 90
+        self.WIDTH = 397
+        self.MOTOR1 = "MOT"#"SR0REB"
+
+        #Variables
+        self.leftMotor = self.motor_boards[self.MOTOR1].motors[0]
+        self.rightMotor = self.motor_boards[self.MOTOR1].motors[1]
+
+        localMarkerIDs = [[i for i in range(100, 120)],
+                          [i for i in range(100, 120)],
+                          [i for i in range(100, 120)],
+                          [i for i in range(100, 120)]]
+        self.palletIDs = localMarkerIDs[self.zone]
+        self.outerHighriseIDs = [i for i in range(195, 198)]
+        self.innerHighriseID = [199]
+
+        #Booleans
+        self.isTargetBox = True #start by looking for box
+        self.targetFound = False
+        self.reachedTarget = False
+        self.linedUp = False
+        self.isTurning = False
+        self.isMoving = False
+        self.scissorLiftUp = False
+        self.grabbed = False
+
+
+    def stop(self):
+        self.leftMotor.power = 0
+        self.rightMotor.power = 0
+        self.isMoving = False
+        self.isTurning = False
+
+    def move(self, speed, distance = None):
+        if distance == None:
+            self.leftMotor.power = speed
+            self.rightMotor.power = speed
+            self.isMoving = True
+        else:
+            distanceMoved = 0
+            startLeftPos = float(self.arduino.command("n"))
+            startRightPos = float(self.arduino.command("y"))
+            while distanceMoved <= distance:
+                currentLeftPos = float(self.arduino.command("n"))
+                currentRightPos = float(self.arduino.command("y"))
+                leftDiff = abs(startLeftPos - currentLeftPos)
+                rightDiff = abs(startLeftPos - currentRightPos)
+                leftDiff = rightDiff  # Needs to be deleted when right encoder works
+                avgDiff = (leftDiff + rightDiff) / 2
+                distanceMoved = avgDiff * self.DIAMETER * math.pi
+                distanceMoved = round(distanceMoved, -2)
+                self.leftMotor.power = speed
+                self.rightMotor.power = speed
+            self.stop()
+
+    def turn(self, speed: float, angle = None):
+        if angle == None:
+            self.leftMotor.power = speed
+            self.rightMotor.power = -speed
+            self.isTurning = True
+
+        else:
+            angleToTurn = angle * self.WIDTH / self.DIAMETER
+            angleTurned = 0
+            startLeftPos = float(self.arduino.command("n"))
+            startRightPos = float(self.arduino.command("y"))
+            while angleTurned <= angle:
+                currentLeftPos = float(self.arduino.command("n"))
+                currentRightPos = float(self.arduino.command("y"))
+                leftDiff = abs(startLeftPos - currentLeftPos)
+                rightDiff = abs(startLeftPos - currentRightPos)
+                leftDiff = rightDiff  # Needs to be deleted when right encoder works
+                avgDiff = (leftDiff + rightDiff) / 2
+                currentAngle = (avgDiff * 2 * math.pi)
+                self.leftMotor.power = speed
+                self.rightMotor.power = -speed
+            self.stop()
+
+    def look(self, targetIDs = None, roll = None):
+        targetInfos = []
+        markers = None
+        markers = self.camera.see()
+        self.targetFound = False
+        if markers != None:
+            if targetIDs == None:
+                return markers
+            for mark in markers:
+                if mark.id in targetIDs:
+                    targetInfos.append(mark)
+                    self.targetFound = True
+
+        if len(targetInfos) > 1 and roll != None:
+            #finalMarker = targetInfos[0]
+            for marker in targetInfos:
+                accRoll = self.roundRollDeg(marker.orientation.roll)
+                if accRoll == roll:
+                    return marker
+
+
+
+        elif targetInfos != []:
+
+            return targetInfos
+
+        else:
+            return None
+
+    def chooseRightSide(self, markers):
+        for mark in markers
+
+    def lineUp(self, targetID):
+        while not self.linedUp:
+            markerInfo = self.look(targetID)
+            if self.targetFound:
+                angleOut = markerInfo.position.horizontal_angle
+                if -0.05 < angleOut < 0.05:
+                    self.linedUp = True
+                    self.stop()
+                    return markerInfo
+                else:
+                    if angleOut < 0:
+                        self.turn(-self.SPEED, abs(angleOut))
+                    else:
+                        self.turn(self.SPEED, angleOut)
+            else:
+                self.turn(0.2)
+                self.look(targetID)
+
+    def roundRollDeg(self, roll):
+        rollDeg = math.degrees(roll)
+        rollDeg = int(90 * round(float(roll) / 90))
+        if rollDeg == -180:
+            rollDeg = 180
+        return rollDeg
+
+    def getYawRad(self, markerInfo):
+        roll_cache = self.roundRollDeg(markerInfo.orientation.roll)
+        if roll_cache < 0 or roll_cache == 180:
+            is_roll_negative = -1
+        else:
+            is_roll_negative = 1
+        if roll_cache == 0 or roll_cache == 180: # or roll_cache == -180:
+            return is_roll_negative * markerInfo.orientation.yaw
+        else:
+            return is_roll_negative * markerInfo.orientation.pitch
+
+    def goToBoxLong(self, targetID):
+        speed = 0.2
+        targetReached = False
+        markerInfo = None
+        while not targetReached:
+            if markerInfo == None:
+                markerInfo = self.lineUp(targetID)
+            else:
+                yaw = self.getYawRad(markerInfo)
+                if yaw < 0:
+                    speed = -speed
+                    yaw = abs(yaw)
+                distance = markerInfo.orientation.yaw
+                distanceAway = math.cos(yaw) * distance
+                distanceTowards = math.sin(yaw) * distance
+                self.turn(speed, yaw)
+                self.move(speed, distanceAway)
+                self.turn(speed, math.pi/2)
+                self.move(self.SPEED, distanceTowards)
+                targetReached = True
+
+    def goToBoxShort(self, markerID):
+
+        targetReached = False
+        markerInfo = None
+        while not targetReached:
+            if markerInfo == None:
+                markerInfo = self.lineUp(markerID)
+            else:
+                distance = markerInfo.position.distance
+                self.move(0.2, distance-25)
+                targetReached = True
+
+
+    def moveGrabber(self, grab: bool):
+
+
+
+        return
+
+
+    def scissorUp(self, height):
+
+
+        return
+
+    def scissorDown(self):
+        return
