8cbe7fe28cd9f2757f7131f973d180f1c601c10c
ARK1309 <ameyakotian19@gmail.com>
2025-02-22 10:58:41 +0000

    Put testing stuff into function so can be called separately to actual code

--- robot.py
+++ robot.py
@@ -6,16 +6,24 @@ myRobot = MyRobot()
 
 while True:
 
-    # myRobot.turn(0.2)
-    # myRobot.targetIDs = myRobot.palletIDs
-    # myRobot.look(80)
-    # print(myRobot.markerInfo)
-    #myRobot.goToBoxLong(80)
-    mark = (myRobot.look(myRobot.palletIDs))
-    if mark != None:
-        print(mark.id)
-        roll = mark.orientation.roll
-        roll = myRobot.roundRollDeg(roll)
-        print(mark.orientation.roll)
-        print(mark.orientation.yaw)
-        print(mark.orientation.pitch)
+        else:
+
+
+def test(myRobot):
+    while True:
+        # myRobot.turn(0.2)
+        # myRobot.targetIDs = myRobot.palletIDs
+        # myRobot.look(80)
+        # print(myRobot.markerInfo)
+        #myRobot.goToBoxLong(80)
+        mark = (myRobot.look(myRobot.palletIDs))
+        if mark != None:
+            print(mark.id)
+            roll = mark.orientation.roll
+            roll = myRobot.roundRollDeg(roll)
+            print(mark.orientation.roll)
+            print(mark.orientation.yaw)
+            print(mark.orientation.pitch)
+
+strat1(myRobot)
+test(myRobot)
