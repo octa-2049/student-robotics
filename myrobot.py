580361489e9075929175d3a81a39ce86d5dc7435
ARK1309 <ameyakotian19@gmail.com>
2025-02-22 10:24:43 +0000

    Added comments

--- myrobot.py
+++ myrobot.py
@@ -90,8 +90,8 @@ class MyRobot(Robot):
         markers = self.camera.see()
         self.targetFound = False
         if markers != None:
-            if targetIDs == None:
-                return markers
+            if targetIDs == None: #If no target but just want to
+                return markers    #see if any markers visible
             for mark in markers:
                 if mark.id in targetIDs:
                     targetInfos.append(mark)
