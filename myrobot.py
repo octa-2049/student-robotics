9b336702aa4c8a9165c651659c7c2cd04851ecc5
ARK1309 <ameyakotian19@gmail.com>
2025-03-23 12:44:51 +0000

    Stuff

--- myrobot.py
+++ myrobot.py
@@ -258,7 +258,7 @@ class MyRobot(Robot):
                     self.stop()
                 else:
                     if angleOut < 0:
-                        self.turn(-self.SPEED, abs(angleOut/4))
+                        self.turn(-self.MAX_SPEED, abs(angleOut / 4))
                     else:
                         self.turn(self.SPEED, angleOut/4)
             else:
