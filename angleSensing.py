16bc26752c546eff61172b2f8d5160b54aa89576
Ameya <ameyakotian19@gmail.com>
2025-06-18 20:18:12 +0100

    Update angleSensing.py (#15)

--- angleSensing.py
+++ angleSensing.py
@@ -1,47 +1,38 @@
 from sr.robot3 import *
-import math
+import cv2
 
 robot = Robot()
 
-'''robot.kch.leds[LED_A].colour = Colour.GREEN
-robot.kch.leds[LED_B].colour = Colour.GREEN
-robot.kch.leds[LED_C].colour = Colour.GREEN
-'''
+'''Turn your robot to the left, to the centre, then to the right, then reverse this.
+During this, illuminate the LEDs based on the following conditions:
 
+LED A in blue when the marker is more than 15° left from square on.
+LED B in red when the marker is approximately square on (within a 30° arc).
+LED C in blue when the marker is more than 15° right from square on.'''
 
 while True:
     markers = robot.camera.see()
-    if markers != None:       
+    if markers != None:      
         for marker in markers:
-            radAngle = marker.position.horizontal_angle
-            degAngle = radAngle * (180/math.pi)
-            distance = marker.position.distance
-            print(radAngle)
-            print(degAngle)
-
-            if degAngle > 15:
+            angleRad = marker.position.horizontal_angle
+            angleDeg = angleRad * 57.296
+            
+            #15deg left from square on (negative) LED A blue 
+            if angleDeg < -15: 
                 robot.kch.leds[LED_A].colour = Colour.BLUE
                 robot.kch.leds[LED_B].colour = Colour.OFF
                 robot.kch.leds[LED_C].colour = Colour.OFF
-                print("Too right")
-            elif degAngle < -15:
-                robot.kch.leds[LED_C].colour = Colour.BLUE
+            
+            #15deg right from squre on (positive) LED C blue
+            elif angleDeg > 15: 
                 robot.kch.leds[LED_A].colour = Colour.OFF
                 robot.kch.leds[LED_B].colour = Colour.OFF
-                print("Too left")
-            elif degAngle < 15 and degAngle > -15:
-                robot.kch.leds[LED_B].colour = Colour.RED
-                robot.kch.leds[LED_A].colour = Colour.OFF
-                robot.kch.leds[LED_C].colour = Colour.OFF
-                print("Just perfect")
+                robot.kch.leds[LED_C].colour = Colour.BLUE
+            
+            #Approx square on LED B red 
             else:
-                robot.kch.leds[LED_B].colour = Colour.WHITE
-                robot.kch.leds[LED_A].colour = Colour.WHITE
-                robot.kch.leds[LED_C].colour = Colour.WHITE
+                robot.kch.leds[LED_A].colour = Colour.OFF
+                robot.kch.leds[LED_B].colour = Colour.RED
+                robot.kch.leds[LED_C].colour = Colour.OFF
                 
-                
-    
-#LED A in blue when the marker is more than 15° left from square on.
-#LED B in red when the marker is approximately square on (within a 30° arc).
-#LED C in blue when the marker is more than 15° right from square on.
 
